3b7ecdafec009edaffb88691c6679252841467b2
Moiz Baloch <khanmoaiz682@gmail.com>
2026-01-01 09:44:56 +0500

    Initial Commit - Initial Setup for application

---
+++ api/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.log(error);
+})
